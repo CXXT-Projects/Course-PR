98e113fc6b39a77367f3f7d5f671956a15911169
ChengDahai <2302078241@qq.com>
2018-11-13 21:27:27 +0800

    remove print function

--- lesson2/src/main/java/com/cxxt/huangyz0918/lesson/Cheng/Sort.java
+++ lesson2/src/main/java/com/cxxt/huangyz0918/lesson/Cheng/Sort.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Sort {
     public static void main(String[] args){
-        int[] a = {1,3,5,4,2,7,6,8,9};
-        bubbleSort(a);
-        System.out.println(Arrays.toString(a));
+//        int[] a = {1,3,5,4,2,7,6,8,9};
+//        bubbleSort(a);
+//        System.out.println(Arrays.toString(a));
     }
 
     public static void bubbleSort(int[] a){
