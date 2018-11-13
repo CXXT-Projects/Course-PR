4ee14bd81bfb5c4d6fb41c93a614e47154de291c
JackWang <544907049@qq.com>
2018-11-13 20:13:20 +0800

    实现了冒泡排序法

--- lesson2/src/main/java/com/cxxt/huangyz0918/lesson/JackWang/Sort.java
+++ lesson2/src/main/java/com/cxxt/huangyz0918/lesson/JackWang/Sort.java
@@ -1,19 +1,24 @@
 package com.cxxt.huangyz0918.lesson.JackWang;
 
+/**
+ * @author JackWang
+ *     Created: Tue Nov 13 15:21:29 CST 2018
+ */
+
 public class Sort {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 8, 6, 4, 5, 7, 9};
-        sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+//        int[] arr = {1, 2, 3, 8, 6, 4, 5, 7, 9};
+//        sort(arr);
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
     }
 
     public static void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                int temp = 0;
+                int temp;
                 if (arr[j + 1] > arr[j]) {
                     temp = arr[j + 1];
                     arr[j + 1] = arr[j];
@@ -22,6 +27,6 @@ public class Sort {
             }
         }
     }
-    
+
 }
 
