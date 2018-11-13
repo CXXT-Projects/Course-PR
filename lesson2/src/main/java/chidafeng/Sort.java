4db70d14e19464d572fb7cc259b538be5d324494
homiec <627417823@qq.com>
2018-11-13 21:10:38 +0800

    sort

--- lesson2/src/main/java/chidafeng/Sort.java
+++ lesson2/src/main/java/chidafeng/Sort.java
@@ -1,4 +1,24 @@
 package chidafeng;
 
 public class Sort {
+    public static void main(String[] args) {
+//        int[] arr = {1, 2, 3, 8, 6, 4, 5, 7, 9};
+//        sort(arr);
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                int temp;
+                if (arr[j + 1] > arr[j]) {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 }
