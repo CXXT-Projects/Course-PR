1c02ee5c773cfec38d90597b4a00824cc4c35be1
huangyz0918 <huangyz0918@gmail.com>
2018-11-13 16:20:32 +0800

    🎨 style: add gradle checks

---
+++ lesson2/src/main/java/com/cxxt/huangyz0918/lesson/Two.java
@@ -0,0 +1,16 @@
+package com.cxxt.huangyz0918.lesson;
+
+
+/**
+ * @author huangyz0918
+ *     Created: Tue Nov 13 15:21:29 CST 2018
+ */
+public class Two {
+  public Two() {
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
