3530d170df42cb158bb45bd11f35c5aac03fc782
ProgrammingMonkey123 <459840918@qq.com>
2018-11-14 19:14:03 +0800

    add some comments

--- lesson2/src/main/java/com/cxxt/huangyz0918/lesson/lizhi/Sort.java
+++ lesson2/src/main/java/com/cxxt/huangyz0918/lesson/lizhi/Sort.java
@@ -1,11 +1,15 @@
 package com.cxxt.huangyz0918.lesson.lizhi;
 
+/**
+ * @author lizhi
+ * */
+
 public class Sort {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 8, 6, 4, 5, 7, 9};
-        sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        //int[] arr = {1, 2, 3, 8, 6, 4, 5, 7, 9};
+        //sort(arr);
+        //for (int i = 0; i < arr.length; i++) {
+          //  System.out.println(arr[i]);
         }
     }
 
