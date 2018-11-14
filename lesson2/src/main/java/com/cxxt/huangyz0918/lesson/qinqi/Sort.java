e46758ee5f482590039e3b8ab00f67b0b087844c
QI_N_QIGT <1515035714@qq.com>
2018-11-14 15:26:20 +0800

    qinqi sorted3

--- lesson2/src/main/java/com/cxxt/huangyz0918/lesson/qinqi/Sort.java
+++ lesson2/src/main/java/com/cxxt/huangyz0918/lesson/qinqi/Sort.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 * */
 public class Sort {
     public static void main(String[] args){
-        int[] t = {1,4,2,6,4,5,8,6};
-        bubbleSort(t);
+//        int[] t = {1,4,2,6,4,5,8,6};
+//        bubbleSort(t);
 //      System.out.println(Arrays.toString(t));
     }
 
